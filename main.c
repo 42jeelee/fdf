f7f66ed40b686f7145cfcb6eb797e77e2a2abaf7
Jeehoon Lee <jeelee@c10r3s4.42seoul.kr>
2023-03-20 16:48:50 +0900

    learning ..

--- main.c
+++ main.c
@@ -6,23 +6,36 @@
 /*   By: jeelee <jeelee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 16:43:49 by jeelee            #+#    #+#             */
-/*   Updated: 2023/03/14 17:28:17 by jeelee           ###   ########.fr       */
+/*   Updated: 2023/03/20 16:47:49 by jeelee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minilibx/mlx.h"
+#include <stdlib.h>
+
+typedef struct s_vars {
+	void	*mlx;
+	void	*win;
+}	t_vars;
+
+int	key_hook(int keycode, t_vars *vars)
+{
+	if (keycode == 53)
+	{
+		mlx_destroy_window(vars->mlx, vars->win);
+		exit(0);
+	}
+	return (0);
+}
 
 int	main(void)
 {
-	void	*mlx_ptr;
-	void	*win_ptr;
-	int		width;
-	int		height;
+	t_vars	vars;
 
-	width = 400;
-	height = 400;
-	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, width, height, "Title");
-	mlx_loop(mlx_ptr);
+	vars.mlx = mlx_init();
+	vars.win = mlx_new_window(vars.mlx, 500, 500, "Title");
+	mlx_key_hook (vars.win, key_hook, &vars);
+	mlx_string_put(vars.mlx, vars.win, 200, 200, 0xFFFFFF, "hello");
+	mlx_loop(vars.mlx);
 	return (0);
 }
