232b6c4b017c97d5553e7343b17a5589057adc38
Jeehoon Lee <jeelee@c10r1s2.42seoul.kr>
2023-03-27 00:50:17 +0900

    add map init

--- set_gap.c
+++ set_gap.c
@@ -6,7 +6,7 @@
 /*   By: jeelee <jeelee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 23:43:58 by jeelee            #+#    #+#             */
-/*   Updated: 2023/03/26 21:44:51 by jeelee           ###   ########.fr       */
+/*   Updated: 2023/03/27 00:49:49 by jeelee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,11 @@ void	set_gap(t_map *map, t_mlx *mlx)
 
 	mlx->gap = 42;
 	max = set_coordinate(mlx->gap, map);
-	while (max.x + WINDOW_MARGIN > WINDOW_MAX_WIDTH || max.y + WINDOW_MARGIN > WINDOW_MAX_HEIGHT)
+	while (max.x + WINDOW_MARGIN > WINDOW_MAX_WIDTH || \
+		max.y + WINDOW_MARGIN > WINDOW_MAX_HEIGHT)
 	{
 		(mlx->gap)--;
+		map_init(map);
 		max = set_coordinate(mlx->gap, map);
 	}
 	mlx->width = max.x + WINDOW_MARGIN;
