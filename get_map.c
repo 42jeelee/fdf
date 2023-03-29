979fd38c0cf5d64e7d0b1d0afddeef9df7b305fe
Jeehoon Lee <jeelee@c10r2s14.42seoul.kr>
2023-03-30 01:33:08 +0900

    fixed sf

--- get_map.c
+++ get_map.c
@@ -6,7 +6,7 @@
 /*   By: jeelee <jeelee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 16:00:58 by jeelee            #+#    #+#             */
-/*   Updated: 2023/03/29 23:35:44 by jeelee           ###   ########.fr       */
+/*   Updated: 2023/03/30 01:32:28 by jeelee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -108,8 +108,9 @@ t_map	*get_map(char *filename)
 	map->high = 0;
 	map->mid = 0;
 	map->mapinit = 0;
+	map->map = 0;
 	fd = open(filename, O_RDONLY);
-	if (read_map(fd, map) == -1)
+	if (fd == -1 || read_map(fd, map) == -1)
 		return (fail_read_map(map, fd));
 	map->map = (t_dot *)malloc(sizeof(t_dot) * (map->width * map->height));
 	if (!map->mapinit)
